b7bb825e5db8e03199388f8e14f491d20a69839d
Sushrutha05 <sushruthavn@gmail.com>
2025-10-28 13:57:16 +0530

    Login Page in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+
+function ButtonComponent({btn_title}){
+  return(
+    <button type="button" title={btn_title}>{btn_title}</button>
+  );
+} 
+
+function TextInput({title, placeholder, type}) {
+  return(
+    <input type={type} title={title} placeholder={placeholder}/>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='project_title'>
+        <h1>Timetable Management</h1>
+      </div>
+      
+      <div className="form-container">
+        <TextInput title="Email" placeholder="Email" type="text"/>
+        <br/>
+        <TextInput title="Password" placeholder="Password" type="password"/>
+        <br />
+        <ButtonComponent btn_title="Login"/>
+      </div>
+      
     </div>
   );
 }
