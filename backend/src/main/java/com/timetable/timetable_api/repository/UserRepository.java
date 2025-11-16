e0368461fcebaa3de8acb777561ecbe33e5e006c
sharmavaibhav31 <sharma31stmay@gmail.com>
2025-11-16 14:18:57 +0530

    Added Repository Interfacess

---
+++ backend/src/main/java/com/timetable/timetable_api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.timetable.timetable_api.repository;
+
+import com.timetable.timetable_api.model.User; // Import your User model
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Declares this as a Spring-managed repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // That's it!
+}
