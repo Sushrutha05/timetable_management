d0ad253bfb29527339aa16ffef3760926be58b72
sharmavaibhav31 <sharma31stmay@gmail.com>
2026-02-28 22:30:06 +0530

    Updated api to parse text for reset

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -23,7 +23,12 @@ async function fetchAPI(endpoint, options = {}) {
     return null;
   }
 
-  return response.json();
+  const contentType = response.headers.get("content-type");
+  if (contentType && contentType.includes("application/json")) {
+    return response.json();
+  } else {
+    return response.text();
+  }
 }
 
 // Admin APIs - Faculty
